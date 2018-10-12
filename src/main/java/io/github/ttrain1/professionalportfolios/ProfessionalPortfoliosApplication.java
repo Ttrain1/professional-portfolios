05aff03c5260da4ef7fb4692f7839e9e13615c11
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-10-12 11:04:31 -0400

    initail commit

---
+++ src/main/java/io/github/ttrain1/professionalportfolios/ProfessionalPortfoliosApplication.java
@@ -0,0 +1,12 @@
+package io.github.ttrain1.professionalportfolios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalPortfoliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalPortfoliosApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/ttrain1/professionalportfolios/ProfessionalPortfoliosApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.ttrain1.professionalportfolios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfessionalPortfoliosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
